7b834dacc86fd80fe403e72666bc5b4930c18b5f
josh21351345 <joshuaa96@naver.com>
2025-06-07 18:03:46 +0900

    Initial commit

---
+++ Source/SpiderPractice/SpiderAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SpiderAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPIDERPRACTICE_API USpiderAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};
